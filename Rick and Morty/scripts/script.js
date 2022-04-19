e8da180e8f4f949bc6fa791b6b098002a9c24ba8
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-04-19 13:53:28 +0200

    update

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -1,24 +1,55 @@
-// async function datafromApisurl(url){
-//     const result = fetch('https://rickandmortyapi.com/api/episode')
-//     const resultbody = await(result)
-//     return resultbody
-//     console.log(result)
-//     resultbody.forEach(element => {
-//         console.log(`results ${element}`)
-        
-//     });
-// }
-function updatemainconatainer(id,name,date,episodecode){
-    const title = document.createElement('h2');
-    title.innerText = name;
-    const mainareaElement =document.querySelector('.maindiv');
-    mainareaElement.appendChild(title);
+
+let mainAreaElement;
+function renderCharacter(name ,status, species, image){
+    const cardCharacterElement = document.createElement('div');
+    mainAreaElement.appendChild(cardCharacterElement);
+    //Add image source
+    const characterImageElement = document.createElement('img');
+    characterImageElement.src = image;
+
+    // Add name
+    const characterNameElement = document.createElement('div');
+    characterNameElement.innerText = name;
+
+    // Add specie and status
+    const characterSpecieStatusElement = document.createElement('div');
+    characterSpecieStatusElement.innerText = `${species} | ${status}`;
+
+    cardCharacterElement.appendChild(characterImageElement);
+    cardCharacterElement.appendChild(characterNameElement);
+    cardCharacterElement.appendChild(characterSpecieStatusElement);
 
 }
+ async function fetchcharacters(characterURLs) {
+    //map all urls to fetch peomises
+    const characterfetchPromises = characterURLs.map(characterURL => fetch(characterURL));
+    // transform the response into json objects
+    const resolvedFetchResponses = await Promise.all(characterfetchPromises);
+     // transform the response into json promises
+     const jsonpromises =resolvedFetchResponses.map(resolvedFetchResponses =>resolvedFetchResponses.json());
+     // transfor json promises into resolved json promise
+     const resolvedJsonPromises = await Promise.all(jsonpromises);
+     console.log(resolvedJsonPromises);
+     resolvedJsonPromises.forEach(characterJson =>renderCharacter(
+        characterJson.name, characterJson.status, characterJson.species, characterJson.image));
 
+}
+// updatecontainer
+function updatemainconatainer(name,date,episodecode,characterURLs){
+    const mainareaElement =document.querySelector('.maindiv');
+    mainareaElement.innerHTML='';
+    const title = document.createElement('h2');
+    title.innerText = name;
+    const releseDateAndCode =document.createElement('p');
+    releseDateAndCode.innerText = `${date} | ${episodecode}`;
+    mainareaElement.appendChild(title);
+    mainareaElement.appendChild(releseDateAndCode);
+
+    fetchcharacters(characterURLs);
+}
 
 const root =document.querySelector('#root');
-
+//maincontainer
 function maincontainer (){
     const maincontainerpage =document.createElement('div');
     maincontainerpage.className ="maindiv";
@@ -26,29 +57,10 @@ function maincontainer (){
     const heading =document.createElement('p');
     maincontainerpage.appendChild(heading);
     root.appendChild(maincontainerpage);
-
-const images = document.createElement('image');
-images.forEach(img =>{
-    
-    
-    images.src ="https://rickandmortyapi.com/api/character/avatar/1.jpeg";
-    
-    const dateof = document.createElement('p');
-    dateof.textContent = 'april 15,2022 |S01E1';
-
-})
-    
-
-
-    //appending
-    maincontainerpage.appendChild(images)
-    
-    
+   
 }
-
 //create sidebar nav
-function sidebar(){
-    
+function sidebar(){   
 fetch('https://rickandmortyapi.com/api/episode')
 .then(result =>{
     return result.json()   
@@ -64,22 +76,23 @@ fetch('https://rickandmortyapi.com/api/episode')
         titleepisode.innerText =`episode ${episode.id}`;
         sidebarnavigation.appendChild(titleepisode);
         titleepisode.addEventListener("click" , _event =>{
-           updatemainconatainer(episode.id,episode.name,episode.air_date,episode.characters)
+           updatemainconatainer(episode.name,episode.air_date,episode.episode, episode.characters);
+        })   
+    }); 
+    const firstEpisode = json.results[0];
+    updatemainconatainer(firstEpisode.name,firstEpisode.air_datefirstEpisode.episode, firstEpisode.characters); 
 
-        })
-        
-    });  
 })
-.catch(error =>{
-    console.error(error);
-    const errormassege =document.createElement('h1');
-    errormassege.innerText="there is a error in serverside";
-    document.querySelector('body').appendChild(errormassege);
-});
+
 }
-
-
-
-
 sidebar();
 maincontainer();
+// promise structure
+const promiseFetch = new Promise((resolve, reject)=>{
+    // getting information by using url
+    const response = 200;
+    if (response !== 200){
+        reject(`Something went wrong, status code is not 200, it is: ${response}`);
+    }
+    resolve(response);
+})
